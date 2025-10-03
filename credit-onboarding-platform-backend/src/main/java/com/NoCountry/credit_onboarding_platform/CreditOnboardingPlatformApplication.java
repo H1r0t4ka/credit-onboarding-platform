7d7b6e347480851fa5c522a65dd3d7eb714753de
Matias <matias.llanos1998@gmail.com>
2025-10-02 23:45:49 -0300

    feat: primer commit del backend

---
+++ credit-onboarding-platform-backend/src/main/java/com/NoCountry/credit_onboarding_platform/CreditOnboardingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.NoCountry.credit_onboarding_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditOnboardingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditOnboardingPlatformApplication.class, args);
+	}
+
+}
---
+++ credit-onboarding-platform-backend/src/test/java/com/NoCountry/credit_onboarding_platform/CreditOnboardingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoCountry.credit_onboarding_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditOnboardingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
